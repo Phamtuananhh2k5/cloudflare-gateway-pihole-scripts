736a7158b70bfab91859f3acb46dc55da94d705b
Phamtuananhh2k5 <phamanh8084@gmail.com>
2026-06-01 03:46:45 +0700

    cleanup temp scripts

--- check_full_rule.js
+++
@@ -1,7 +0,0 @@
-import { requestGateway } from "./lib/helpers.js";
-
-const { result: rules } = await requestGateway("/rules", { method: "GET" });
-for (const rule of rules) {
-  console.log("---");
-  console.log(JSON.stringify(rule, null, 2));
-}
--- verify_rule.js
+++
@@ -1,8 +0,0 @@
-import { requestGateway } from "./lib/helpers.js";
-
-const { result: rules } = await requestGateway("/rules", { method: "GET" });
-for (const r of rules) {
-  if (r.name.includes("Allow") || r.name.includes("CGPS")) {
-    console.log(`"${r.name}": precedence=${r.precedence}, action=${r.action}, enabled=${r.enabled}`);
-  }
-}
