226620978dd0d7538fb9a9eba9edc4ef1d08acf3
Phamtuananhh2k5 <phamanh8084@gmail.com>
2026-06-01 03:35:54 +0700

    temp: full rule details

--- check_full_rule.js
+++ check_full_rule.js
@@ -2,5 +2,6 @@ import { requestGateway } from "./lib/helpers.js";
 
 const { result: rules } = await requestGateway("/rules", { method: "GET" });
 for (const rule of rules) {
-  console.log(`"${rule.name}": priority=${rule.priority}, action=${rule.action}, enabled=${rule.enabled}`);
+  console.log("---");
+  console.log(JSON.stringify(rule, null, 2));
 }
